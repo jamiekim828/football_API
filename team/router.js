c42a3558bcea89ded0069a6aabf21a797a772167
jamiekim828 <jamiekim828@gmail.com>
2019-06-26 16:41:24 +0200

    put, update add

--- team/router.js
+++ team/router.js
@@ -4,19 +4,36 @@ const router = new Router()
 
 router.get('/team', function (req, res, next) {
     Team
-    .findAll()
-    .then(teams => {
-        res.send(teams)
-    }
-    )
-    .catch(err => next(err))
+        .findAll(team)
+        .then(teams => {
+            res.send(teams)
+        }
+        )
+        .catch(err => next(err))
+})
+
+router.get('/team/:id', function (req, res, next) {
+    const id = req.params.id
+    Team
+        .findByPk(id)
+        .then(team => res.send(team))
+        .catch(err => next(err))
 })
 
 router.post('/team', function (req, res, next) {
     Team
-    .create(req.body)
-    .then(team => res.json(team))
-    .catch(err => next(err))
+        .create(req.body)
+        .then(team => res.json(team))
+        .catch(err => console.log(err))
+})
+
+router.put('/team/:id', function (req, res, next) {
+    const id = req.params.id
+    Team
+        .findByPk(id)
+        .then(team => team.update(req.body))
+        .then(team => res.send(team))
+        .catch(err => console.log(err))
 })
 
 module.exports = router
