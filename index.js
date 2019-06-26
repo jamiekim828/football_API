9a5be7062a4db3e1b596968af5ddd00ee7944a33
jamiekim828 <jamiekim828@gmail.com>
2019-06-26 17:58:19 +0200

    player add

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const sequelize = require('./db')
 const db = require('./db')
 const team = require('./team/model')
+const player = require('./player/model')
 const teamRouter = require('./team/router')
+const playerRouter = require('./player/router')
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 
 const app = express()
 app.use(jsonParser)
 app.use(teamRouter)
+app.use(playerRouter)
 
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ player/router.js
@@ -0,0 +1,38 @@
+const { Router } = require('express')
+const Player = require('./model')
+const router = new Router()
+
+router.get('/player', function (req, res, next) {
+    Player
+        .findAll()
+        .then(players => {
+            res.send(players)
+        })
+        .catch(err => next(err))
+})
+
+router.get('/player/:id', function (req, res, next) {
+    const id = req.params.id
+    Player
+        .findByPk(id)
+        .then(player => res.send(player))
+        .catch(err => next(err))
+})
+
+router.post('/player', function (req, res, next) {
+    Player
+        .create(req.body)
+        .then(player => res.json(player))
+        .catch(err => console.log(err))
+})
+
+router.put('/player/:id', function (req, res, next) {
+    const id = req.params.id
+    Player
+        .findByPk(id)
+        .then(player => player.update(req.body))
+        .then(player => res.send(player))
+        .catch(err => console.log(err))
+})
+
+module.exports = router
